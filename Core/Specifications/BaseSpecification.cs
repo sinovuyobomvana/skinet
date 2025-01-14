528b960b70c49799c819263fd6f99e29c98110fc
Sinovuyo Bomvana <sinovuyobomvana2@gmail.com>
2025-01-15 00:31:23 +0200

    End of Section 5

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -20,6 +20,22 @@ namespace Core.Specifications
 
         public bool isDistinct { get; private set; }
 
+        public int Take { get; private set; }
+
+        public int Skip { get; private set; }
+
+        public bool isPagingEnabled { get; private set; }
+
+        public IQueryable<T> ApplyCriteria(IQueryable<T> query)
+        {
+            if(criteria != null)
+            {
+                query = query.Where(criteria);
+            } 
+
+            return query;
+        }
+
         protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
         {
             OrderBy = orderByExpression;
@@ -28,10 +44,15 @@ namespace Core.Specifications
         {
             OrderByDescending = orderByDescExpression;
         }
-
         protected void ApplyDistinct()
         {
             isDistinct = true;
+        }
+        protected void ApplyPaging(int skip, int take)
+        {
+            Skip = skip;
+            Take = take;
+            isPagingEnabled = true;
         }
     }
 
